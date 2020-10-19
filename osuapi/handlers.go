30f9150a9b847be95fc238c8405cb236abdb3fbe
ShaddyDC <shaddythefirst@gmail.com>
2020-10-19 18:14:45 +0200

    implement map files

--- osuapi/handlers.go
+++ osuapi/handlers.go
@@ -20,11 +20,12 @@ func (f APIFunc) ServeAPI(osuAPI *OsuAPI, api string, token string) (string, err
 
 func (api *OsuAPI) setupHandlers() {
 	api.Handlers = make(map[string]APIFunc)
-	api.Handlers["/api/user/"] = getUserFunc
+	api.Handlers["/api/v1/user/"] = getUserFunc
+	api.Handlers["/api/v1/map/"] = getMapFunc
 }
 
 func getUserFunc(osuAPI *OsuAPI, path string, token string) (string, error) {
-	id := strings.TrimPrefix(path, "/api/user/")
+	id := strings.TrimPrefix(path, "/api/v1/user/")
 	fmt.Println("Requesting user", id)
 
 	user, err := osuAPI.GetUser(token, id)
@@ -32,4 +33,15 @@ func getUserFunc(osuAPI *OsuAPI, path string, token string) (string, error) {
 		return "", fmt.Errorf("{Error:\"Error with api call: %v\"}", err)
 	}
 	return string(user), nil
+}
+
+func getMapFunc(osuAPI *OsuAPI, path string, token string) (string, error) {
+	id := strings.TrimPrefix(path, "/api/v1/map/")
+	fmt.Println("Requesting map", id)
+
+	m, err := osuAPI.GetMap(id)
+	if err != nil {
+		return "", fmt.Errorf("{Error:\"Error with api call: %v\"}", err)
+	}
+	return string(m), nil
 }
